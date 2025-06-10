bf1a8f3e68366af180f386b6a95ff181214ee8b6
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2025-06-09 19:12:35 -0500

    form submission

--- index.js
+++ index.js
@@ -18,20 +18,60 @@ if (urlParams.has("page")) {
 }
 
 
-async function fetchData() {
-    try {
-        let response = await fetch(`https://j1sonplaceholder.typicode.com/todos?bacon=fresh`)
-        console.log('Response: ', response)
-        let json = await response.json()
-        console.log('JSON Response: ', json)
+// async function fetchData() {
+//     try {
+//         let response = await fetch(`https://j1sonplaceholder.typicode.com/todos?bacon=fresh`)
+//         console.log('Response: ', response)
+//         let json = await response.json()
+//         console.log('JSON Response: ', json)
 
-        if (json.length < 1) {
-            console.log('No data found')
+//         if (json.length < 1) {
+//             console.log('No data found')
+//         }
+//     } catch (error) {
+//         console.error('Error fetching data: ', error)
+//         alert('Error fetching data: ' + error.message)
+//     }
+// }
+
+// fetchData()
+
+
+document.getElementById('myForm').addEventListener('submit', function () {
+    async function submitForm(event) {
+        event.preventDefault() // Prevent the default form submission
+        const form = event.target;
+
+        const formData = {
+            name: form.name.value,
+            email: form.email.value
         }
-    } catch (error) {
-        console.error('Error fetching data: ', error)
-        alert('Error fetching data: ' + error.message)
-    }
-}
 
-fetchData()
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: 'javascript',
+                    body: 'made easy',
+                    userId: 1,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                },
+            })
+
+            // if (!response.ok) {
+            //     throw new Error("Network response was not ok")
+            // }
+
+            const data = await response.json()
+
+            console.log("Form submitted successfully: ", data)
+        } catch (error) {
+            console.error("Error submitting form: ", error)
+            alert("Error submitting form: " + error.message)
+        }
+    }
+
+    submitForm(event)
+})
